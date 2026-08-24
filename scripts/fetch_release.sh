8c0a9999a0d4ddee89c3c70116e4aa7966812a49
Gytjftjygt <92408804+apkile78@users.noreply.github.com>
2026-08-24 16:07:42 -0400

    Fix formatting in fetch_release.sh for git clone command

--- scripts/fetch_release.sh
+++ scripts/fetch_release.sh
@@ -34,7 +34,6 @@ if [ -d "$SOURCE_DIR" ]; then
 fi  
   
 echo "Cloning ${CDDA_REPO} branch ${CDDA_BRANCH}..."  
-git clone --depth 1 --branch "$CDDA_BRANCH" \  
-  "https://github.com/${CDDA_REPO}.git" "$SOURCE_DIR"  
+git clone --depth 1 --branch "$CDDA_BRANCH" "https://github.com/${CDDA_REPO}.git" "$SOURCE_DIR"
   
 echo "Source fetched successfully to: $SOURCE_DIR"
